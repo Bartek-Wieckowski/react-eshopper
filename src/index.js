f5215785c8d928c71f6259e25fbbc620caa04889
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-07-27 15:07:10 +0200

    added FilterProvider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ProductsProvider } from "./contexts/ProductsContext";
+import { FilterProvider } from "./contexts/FilterContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ProductsProvider>
-      <App />
+      <FilterProvider>
+        <App />
+      </FilterProvider>
     </ProductsProvider>
   </React.StrictMode>
 );
