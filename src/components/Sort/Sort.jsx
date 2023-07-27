23ace5e44015b2f25f78063f6bd20908a86ed621
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-07-27 17:07:51 +0200

    added sorted products to select options

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -2,16 +2,16 @@ import { useFilter } from "../../contexts/FilterContext";
 import "./sort.css";
 
 export default function Sort() {
-  const { products, gridView, setGridView, setListView } = useFilter();
+  const { products, gridView, setGridView, setListView, sort, updateSort } = useFilter();
   return (
     <div className="sort-wrapper">
       <div className="btn-container">
-        <button onClick={setGridView}>
+        <button onClick={setGridView} className={`${gridView ? "active" : null}`}>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
             <path d="M104,40H56A16,16,0,0,0,40,56v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,104,40Zm0,64H56V56h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,200,40Zm0,64H152V56h48v48Zm-96,32H56a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V152A16,16,0,0,0,104,136Zm0,64H56V152h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V152A16,16,0,0,0,200,136Zm0,64H152V152h48v48Z"></path>
           </svg>
         </button>
-        <button onClick={setListView}>
+        <button onClick={setListView} className={`${!gridView ? "active" : null}`}>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
             <path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
           </svg>
@@ -21,8 +21,8 @@ export default function Sort() {
       <hr />
       <form>
         <label htmlFor="sort">sort by</label>
-        <select name="sort" id="id" className="sort-input">
-          <option value="price-lowset">price (lowest)</option>
+        <select name="sort" id="id" value={sort} onChange={updateSort} className="sort-input">
+          <option value="price-lowest">price (lowest)</option>
           <option value="price-highest">price (highest)</option>
           <option value="name-a">name (a-z)</option>
           <option value="name-z">name (z-a)</option>
