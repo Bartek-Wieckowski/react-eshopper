9229ed12f1665bea0bb36223229a5ca3c91baa9c
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-07-27 14:38:16 +0200

    created FilterContext

---
+++ src/contexts/FilterContext.js
@@ -0,0 +1,68 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { useProducts } from "./ProductsContext";
+
+const FilterContext = createContext();
+
+const initialState = {
+  filteredProducts: [],
+  allProducts: [],
+  gridView: true,
+  sort: "price-lowest",
+  filters: {
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
+    minPrice: 0,
+    maxPrice: 0,
+    price: 0,
+    shipping: false,
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loadProducts":
+      return { ...state, allProducts: [...action.payload], filteredProducts: [...action.payload] };
+    case "setGridView":
+      return { ...state, gridView: true };
+    case "setListView":
+      return { ...state, gridView: false };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
+function FilterProvider({ children }) {
+  const { products } = useProducts();
+  const [{ gridView, allProducts, filteredProducts }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(
+    function () {
+      dispatch({ type: "loadProducts", payload: products });
+    },
+    [products]
+  );
+
+  function setGridView() {
+    dispatch({ type: "setGridView" });
+  }
+  function setListView() {
+    dispatch({ type: "setGridView" });
+  }
+
+  return (
+    <FilterContext.Provider value={{ gridView, allProducts, filteredProducts, setGridView, setListView }}>
+      {children}
+    </FilterContext.Provider>
+  );
+}
+
+function useFilter() {
+  const context = useContext(FilterContext);
+  if (context === undefined) throw new Error("FilterContext was used outside the FilterProvider");
+  return context;
+}
+
+export { FilterProvider, useFilter };
