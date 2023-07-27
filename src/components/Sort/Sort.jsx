2518f17333451f64597b45ba6e8bc641d183144d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-07-27 15:24:48 +0200

    added onClick function to change display list/grid view

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,21 +1,23 @@
+import { useFilter } from "../../contexts/FilterContext";
 import "./sort.css";
 
 export default function Sort() {
+  const { products, gridView, setGridView, setListView } = useFilter();
   return (
     <div className="sort-wrapper">
       <div className="btn-container">
-        <button>
+        <button onClick={setGridView}>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
             <path d="M104,40H56A16,16,0,0,0,40,56v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,104,40Zm0,64H56V56h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,200,40Zm0,64H152V56h48v48Zm-96,32H56a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V152A16,16,0,0,0,104,136Zm0,64H56V152h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V152A16,16,0,0,0,200,136Zm0,64H152V152h48v48Z"></path>
           </svg>
         </button>
-        <button>
+        <button onClick={setListView}>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
             <path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
           </svg>
         </button>
       </div>
-      <p> products found</p>
+      <p>{products.length} products found</p>
       <hr />
       <form>
         <label htmlFor="sort">sort by</label>
