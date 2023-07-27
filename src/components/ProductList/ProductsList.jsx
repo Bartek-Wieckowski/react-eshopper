f3e8aefc7a259cd8e1214eef1a8164ece2d183df
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-07-27 15:10:13 +0200

    add ListView to if statement

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -1,12 +1,11 @@
 import "./product-list.css";
-import { useProducts } from "../../contexts/ProductsContext";
+// import { useProducts } from "../../contexts/ProductsContext";
 import GridView from "../GridView/GridView";
+import ListView from "../ListView/ListView";
+import { useFilter } from "../../contexts/FilterContext";
 
 export default function ProductsList() {
-  const { products } = useProducts();
-  return (
-    <>
-      <GridView products={products}/>
-    </>
-  );
+  const { products, gridView } = useFilter();
+
+  return gridView === false ? <ListView products={products} /> : <GridView products={products} />;
 }
