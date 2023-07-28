9827446a7e9abd821f97175b52929b719ebd1c89
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-07-28 15:44:08 +0200

    added if statment to display user information if products not found

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -7,5 +7,7 @@ import { useFilter } from "../../contexts/FilterContext";
 export default function ProductsList() {
   const { filteredProducts: products, gridView } = useFilter();
 
+  if(products.length < 1) return <h5 style={{textTransform: "none"}}>Sorry, no products matched your search</h5>
+
   return gridView === false ? <ListView products={products} /> : <GridView products={products} />;
 }
