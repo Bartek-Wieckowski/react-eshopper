64ac04c846959d58187ff56cef741fa047f4c780
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-07-31 13:03:14 +0200

    added single product page route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import PageNotFound from "./pages/PageNotFound";
+import SingleProduct from "./pages/SingleProduct";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/products" element={<Products />}></Route>
+        <Route path="/products/:id" element={<SingleProduct />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
