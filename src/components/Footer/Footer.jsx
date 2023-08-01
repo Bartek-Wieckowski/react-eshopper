a546078f0cdf2717cbc8f0dd78887d0a09c5fa4f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-01 15:56:12 +0200

    added className to footer tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './footer.css'
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer-section">
         <small>Project made only with the assumption of React library practice</small>
         <small>Project design ©Comfy Sloth author John Smilga</small>
         <small>Thanks for your experience and the opportunity to learn from this project</small>
