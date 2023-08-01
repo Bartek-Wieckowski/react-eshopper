8c5af2d1f9556bfb08dd8b0c2cff20270fd3ed0d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-01 15:54:09 +0200

    created special Footer component to info about this project and thanks to project design and author

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+        <small>Project made only with the assumption of React library practice</small>
+        <small>Project design ©Comfy Sloth author John Smilga</small>
+        <small>Thanks for your experience and the opportunity to learn from this project</small>
+    </footer>
+  )
+}
