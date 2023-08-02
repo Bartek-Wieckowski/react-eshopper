fa4e534bd0ec987bc574a956e801095e91c753d6
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-02 12:06:12 +0200

    use props in onCLick and display amount

--- src/components/AmountBtns/AmountBtns.jsx
+++ src/components/AmountBtns/AmountBtns.jsx
@@ -1,15 +1,15 @@
 import "./amount-btns.css";
 
-export default function AmountBtns() {
+export default function AmountBtns({ inc, dec, amount }) {
   return (
     <div className="amount-btns-wrapper">
-      <button type="button" className="amount-btn">
+      <button type="button" className="amount-btn" onClick={dec}>
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
           <path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128Z"></path>
         </svg>
       </button>
-      <h2>0</h2>
-      <button type="button" className="amount-btn">
+      <h2>{amount}</h2>
+      <button type="button" className="amount-btn" onClick={inc}>
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
           <path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path>
         </svg>
