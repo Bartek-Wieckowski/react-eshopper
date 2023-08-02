901fad7d4981765b1a10eaf57b6002452f55580c
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-02 13:27:00 +0200

    added CartProvider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { ProductsProvider } from "./contexts/ProductsContext";
 import { FilterProvider } from "./contexts/FilterContext";
+import { CartProvider } from "./contexts/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ProductsProvider>
       <FilterProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </FilterProvider>
     </ProductsProvider>
   </React.StrictMode>
