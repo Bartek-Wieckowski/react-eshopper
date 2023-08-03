2902336697b7fd6d208f9500050aedfb4892d895
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-03 20:29:20 +0200

    added all css class and finished jsx template

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,5 +1,34 @@
+import { formatPrice } from '../../utils/helpers';
 import './cart-item.css';
+import AmountBtns from '../AmountBtns/AmountBtns';
 
 export default function CartItem({ id, image, name, color, price, amount }) {
-  return <div>{id}</div>;
+  return (
+    <div className="cart-item-wrapper">
+      <div className="title">
+        <img src={image} alt={name} />
+        <div>
+          <h5 className="name">{name}</h5>
+          <p className="color">
+            color :<span style={{ backgroundColor: { color } }}></span>
+          </p>
+          <h5 className="price-small">{formatPrice(price)}</h5>
+        </div>
+      </div>
+      <h5 className="price">{formatPrice(price)}</h5>
+      <AmountBtns amount={amount} />
+      <h5 className="subtotal">{formatPrice(price * amount)}</h5>
+      <button className="remove-btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="tomato"
+          viewBox="0 0 256 256"
+        >
+          <path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path>
+        </svg>
+      </button>
+    </div>
+  );
 }
