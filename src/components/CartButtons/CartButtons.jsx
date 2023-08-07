e6b08dea17be75e1e24669b26f604f4ce1ac9834
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-07 20:24:30 +0200

    disabled login button in navigation

--- src/components/CartButtons/CartButtons.jsx
+++ src/components/CartButtons/CartButtons.jsx
@@ -1,26 +1,44 @@
-import { Link } from "react-router-dom";
-import "./cart-buttons.css";
-import { useCart } from "../../contexts/CartContext";
-import { useMenuContext } from "../../contexts/MenuContext";
-
+import { Link } from 'react-router-dom';
+import './cart-buttons.css';
+import { useCart } from '../../contexts/CartContext';
+import { useMenuContext } from '../../contexts/MenuContext';
+import { useState } from 'react';
 
 export default function CartButtons() {
   const { totalItems } = useCart();
-  const {closeMenu} = useMenuContext();
+  const { closeMenu } = useMenuContext();
+
+  const [isDisabled, setIsDisabled] = useState(true);
   return (
     <div className="cart-btn-wrapper">
       <Link className="cart-btn" to="/cart" onClick={closeMenu}>
         Cart
         <span className="cart-container">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="#fff"
+            viewBox="0 0 256 256"
+          >
             <path d="M96,216a16,16,0,1,1-16-16A16,16,0,0,1,96,216Zm88-16a16,16,0,1,0,16,16A16,16,0,0,0,184,200ZM231.65,74.35l-28.53,92.71A23.89,23.89,0,0,1,180.18,184H84.07A24.11,24.11,0,0,1,61,166.59L24.82,40H8A8,8,0,0,1,8,24H24.82A16.08,16.08,0,0,1,40.21,35.6L48.32,64H224a8,8,0,0,1,7.65,10.35ZM213.17,80H52.89l23.49,82.2a8,8,0,0,0,7.69,5.8h96.11a8,8,0,0,0,7.65-5.65Z"></path>
           </svg>
           <span className="cart-value">{totalItems}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
+      <button
+        type="button"
+        className={`auth-btn ${isDisabled ? 'disabled' : ''}`}
+        disabled={isDisabled}
+      >
         Login
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="#fff"
+          viewBox="0 0 256 256"
+        >
           <path d="M168,56a8,8,0,0,1,8-8h16V32a8,8,0,0,1,16,0V48h16a8,8,0,0,1,0,16H208V80a8,8,0,0,1-16,0V64H176A8,8,0,0,1,168,56Zm62.56,54.68a103.92,103.92,0,1,1-85.24-85.24,8,8,0,0,1-2.64,15.78A88.07,88.07,0,0,0,40,128a87.62,87.62,0,0,0,22.24,58.41A79.66,79.66,0,0,1,98.3,157.66a48,48,0,1,1,59.4,0,79.66,79.66,0,0,1,36.06,28.75A87.62,87.62,0,0,0,216,128a88.85,88.85,0,0,0-1.22-14.68,8,8,0,1,1,15.78-2.64ZM128,152a32,32,0,1,0-32-32A32,32,0,0,0,128,152Zm0,64a87.57,87.57,0,0,0,53.92-18.5,64,64,0,0,0-107.84,0A87.57,87.57,0,0,0,128,216Z"></path>
         </svg>
       </button>
